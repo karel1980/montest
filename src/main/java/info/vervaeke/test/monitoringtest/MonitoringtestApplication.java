c84454dde323a61e0e5db66a3fa0898372a39a9a
Karel Vervaeke <karel.vervaeke@securex.be>
2018-06-08 21:27:41 +0200

    Initial commit

---
+++ src/main/java/info/vervaeke/test/monitoringtest/MonitoringtestApplication.java
@@ -0,0 +1,12 @@
+package info.vervaeke.test.monitoringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringtestApplication.class, args);
+	}
+}
